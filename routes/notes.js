efd3e37d910b2ebdb540c521aa0f5b4b5fdf073c
Alexis Brownstein <alexis@drdatabase.com>
2022-12-13 09:40:45 -0500

    corrected path to json.db, added pages folder

--- routes/notes.js
+++ routes/notes.js
@@ -8,13 +8,13 @@ const {
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-  readFromFile('../db/notes.json').then((data) => res.json(JSON.parse(data)));
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // GET Route for a specific note
 notes.get('/:note_id', (req, res) => {
   const noteId = req.params.note_id;
-  readFromFile('../db/notes.json')
+  readFromFile('./db/notes.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       const result = json.filter((note) => note.note_id === noteId);
@@ -27,14 +27,14 @@ notes.get('/:note_id', (req, res) => {
 // DELETE Route for a specific note
 notes.delete('/:note_id', (req, res) => {
   const noteId = req.params.note_id;
-  readFromFile('../db/notes.json')
+  readFromFile('./db/notes.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       // Make a new array of all notes except the one with the ID provided in the URL
       const result = json.filter((note) => note.note_id !== noteId);
 
       // Save that array to the filesystem
-      writeToFile('../db/notes.json', result);
+      writeToFile('./db/notes.json', result);
 
       // Respond to the DELETE request
       res.json(`Item ${noteId} has been deleted 🗑️`);
@@ -54,7 +54,7 @@ notes.post('/', (req, res) => {
       note_id: uuidv4(),
     };
 
-    readAndAppend(newNote, '../db/notes.json');
+    readAndAppend(newNote, './db/notes.json');
     res.json(`note added successfully 🚀`);
   } else {
     res.error('Error in adding note');
--- server.js
+++ server.js
@@ -21,17 +21,17 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/404.html'))
+  res.sendFile(path.join(__dirname, '/public/pages/404.html'))
 );
 
 
